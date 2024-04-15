9f6ab267d4b062ad1dbf910c85d5b46a8628837b
TheTacBanana <erisbrabham@gmail.com>
2024-04-15 14:38:11 +0100

    Add login screen

--- nixos/wm.nix
+++ nixos/wm.nix
@@ -2,11 +2,17 @@
 {
     programs.hyprland.enable = true;
 
+    services.xserver.enable = true;
+    services.xserver.displayManager.sddm.enable = true;
+    services.xserver.displayManager.sddm.wayland.enable = true;
+    services.xserver.displayManager.sddm.theme = "where_is_my_sddm_theme";
+
     security.pam.services.swaylock = {};
 
     environment.systemPackages = with pkgs; [
         brightnessctl
         swayidle
         swaylock
+        where-is-my-sddm-theme
     ];
 }
