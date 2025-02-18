f55b142168ec2693dfe47af289b43fcfeebe66d1
TheTacBanana <github@vivianeris.com>
2025-02-18 16:00:59 +0000

    Merge core things

--- nixos/bananatop.nix
+++ nixos/bananatop.nix
@@ -1,20 +1,21 @@
 { config, pkgs, ... }:
 {
-    imports = [
-        ./core.nix
-        ./gpu.nix
-        ./hyprland.nix
-        ./git.nix
-        ./languages.nix
-        ./audio.nix
-        ./bluetooth.nix
-        ./helix.nix
-    ];
+  imports = [
+    ./core.nix
+    ./gpu.nix
+    ./steam.nix
+    ./hyprland.nix
+    ./git.nix
+    ./languages.nix
+    ./audio.nix
+    ./bluetooth.nix
+    ./helix.nix
+  ];
 
-    networking.hostName = "bananatop";
+  networking.hostName = "bananatop";
 
-    system.stateVersion = "24.11";
+  system.stateVersion = "24.11";
 
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 }
--- nixos/core.nix
+++ nixos/core.nix
@@ -1,126 +1,134 @@
-{ config, pkgs, ... }:
 let
-  nix-alien-pkgs = import (
-    builtins.fetchTarball "https://github.com/thiagokokada/nix-alien/tarball/master"
-  ) { };
-  pinnedZoomPkgs = import (builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/0c19708cf035f50d28eb4b2b8e7a79d4dc52f6bb.tar.gz";
-      sha256 = "0ngw2shvl24swam5pzhcs9hvbwrgzsbcdlhpvzqc7nfk8lc28sp3";
-  }) {
-    system = "x86_64-linux";
-    config.allowUnfree = true;
-  };
-
+  pinnedZoomPkgs =
+    import
+      (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/0c19708cf035f50d28eb4b2b8e7a79d4dc52f6bb.tar.gz";
+        sha256 = "0ngw2shvl24swam5pzhcs9hvbwrgzsbcdlhpvzqc7nfk8lc28sp3";
+      })
+      {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
   pinnedZoom = pinnedZoomPkgs.zoom-us;
 in
+{ pkgs, ... }:
 {
-    time.timeZone = "Europe/London";
+  time.timeZone = "Europe/London";
 
-    networking = {
-        networkmanager.enable = true;
-        nameservers = [ "1.1.1.1" "8.8.8.8" ];
-    };
-
-    i18n.defaultLocale = "en_GB.UTF-8";
-    i18n.extraLocaleSettings = {
-        LC_ADDRESS = "en_GB.UTF-8";
-        LC_IDENTIFICATION = "en_GB.UTF-8";
-        LC_MEASUREMENT = "en_GB.UTF-8";
-        LC_MONETARY = "en_GB.UTF-8";
-        LC_NAME = "en_GB.UTF-8";
-        LC_NUMERIC = "en_GB.UTF-8";
-        LC_PAPER = "en_GB.UTF-8";
-        LC_TELEPHONE = "en_GB.UTF-8";
-        LC_TIME = "en_GB.UTF-8";
-    };
-
-    users.users.banana = {
-        isNormalUser = true;
-        description = "Eris";
-        extraGroups = [ "networkmanager" "wheel" "storage" "gamemode" ];
-    };
-
-    nixpkgs.config.allowUnfree = true;
-    system.autoUpgrade.enable = true;
-
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-    programs.thunar.enable = true;
-    services.gvfs.enable = true; # Mount, trash, and other functionalities
-    services.tumbler.enable = true; # Thumbnail support for images
-    programs.thunar.plugins = with pkgs.xfce; [
-        thunar-archive-plugin
+  networking = {
+    networkmanager.enable = true;
+    nameservers = [
+      "1.1.1.1"
+      "8.8.8.8"
     ];
-    programs.file-roller.enable = true;
+  };
 
-    environment.systemPackages = with pkgs; [
-        vim
-        kitty
-        btop
-        gnumake
-        xdg-utils
-        cloc
-        networkmanagerapplet
-        firefox
-        google-chrome
-        pympress
-        vscode-fhs
-        qbittorrent
-        vlc
-        zip
-        unzip
-        imhex
-        gparted
-        spotify
-        discord
-        discord-canary
-        pinnedZoom
-        nix-alien-pkgs.nix-alien
+  environment.systemPackages = with pkgs; [
+    vim
+    kitty
+    btop
+    gnumake
+    xdg-utils
+    cloc
+    networkmanagerapplet
+    firefox
+    google-chrome
+    pympress
+    vscode-fhs
+    qbittorrent
+    vlc
+    zip
+    unzip
+    imhex
+    gparted
+    spotify
+    discord
+    discord-canary
+    pinnedZoom
+  ];
 
-        lxqt.lxqt-policykit
+  i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
+  users.users.banana = {
+    isNormalUser = true;
+    description = "Eris";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "storage"
+      "gamemode"
     ];
+  };
 
-    fonts.packages = with pkgs; [
-        fira-code
+  nixpkgs.config.allowUnfree = true;
+  system.autoUpgrade.enable = true;
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+
+  programs.thunar.enable = true;
+  services.gvfs.enable = true; # Mount, trash, and other functionalities
+  services.tumbler.enable = true; # Thumbnail support for images
+  programs.thunar.plugins = with pkgs.xfce; [
+    thunar-archive-plugin
+  ];
+  programs.file-roller.enable = true;
+  fonts.packages = with pkgs; [
+    fira-code
+  ];
+
+  # Keyring
+  security.pam.services.gdm.enableGnomeKeyring = true;
+  services.gnome.gnome-keyring.enable = true;
+  programs.seahorse.enable = true;
+  security.polkit.enable = true;
+
+  security.sudo = {
+    enable = true;
+    extraRules = [
+      {
+        commands = [
+          {
+            command = "${pkgs.systemd}/bin/systemctl suspend";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "${pkgs.systemd}/bin/reboot";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "${pkgs.systemd}/bin/poweroff";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "${pkgs.systemd}/bin/nixos-rebuild";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+        groups = [ "wheel" ];
+      }
     ];
+  };
 
-    # Keyring
-    security.pam.services.gdm.enableGnomeKeyring = true;
-    services.gnome.gnome-keyring.enable = true;
-    programs.seahorse.enable = true;
-    security.polkit.enable = true;
+  programs.nix-ld = {
+    enable = true;
+  };
 
-    security.sudo = {
-        enable = true;
-        extraRules = [{
-            commands = [
-            {
-                command = "${pkgs.systemd}/bin/systemctl suspend";
-                options = [ "NOPASSWD" ];
-            }
-            {
-                command = "${pkgs.systemd}/bin/reboot";
-                options = [ "NOPASSWD" ];
-            }
-            {
-                command = "${pkgs.systemd}/bin/poweroff";
-                options = [ "NOPASSWD" ];
-            }
-            {
-                command = "${pkgs.systemd}/bin/nixos-rebuild";
-                options = [ "NOPASSWD" ];
-            }
-            ];
-            groups = [ "wheel" ];
-        }];
-    };
-
-    programs.nix-ld = {
-        enable = true;
-        # libraries = pkgs.steam-run.fhsenv.args.multiPkgs pkgs;
-    };
-
-    programs.bash.shellAliases = {
-        bat = "cat /sys/class/power_supply/BAT0/capacity";
-    };
+  programs.bash.shellAliases = {
+    bat = "cat /sys/class/power_supply/BAT0/capacity";
+    cloc = "cloc --vcs=git";
+  };
 }
